2848f2f9aae2a3b8339bbaacfcfd86dc7b15a228
Ralf <ralf.kellner@gmx.de>
2025-06-17 17:05:22 +0200

    update from Mac OS

--- 01_Filtering/01_filtering_EU.py
+++ 01_Filtering/01_filtering_EU.py
@@ -7,10 +7,10 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-data_path      = r"/Users/ralfkellner/Datastream/US"
+data_path      = r"/Users/ralfkellner/Datastream/EU"
 save_dir = "Filtered_Data"
 os.makedirs(os.path.join(data_path, save_dir))
-nof_subfolders = 31 
+nof_subfolders = 54 
 
 timeseries_dfs = []
 static_dfs     = []
@@ -18,7 +18,7 @@ static_dfs     = []
 for i in tqdm(range(1, nof_subfolders + 1), desc="Load data"):
     folder_nbr       = f"{i:02d}"
     folder_path      = os.path.join(data_path, folder_nbr)
-    OHLCV_panel_iter = pd.read_feather(os.path.join(data_path, "Panel_Data_US", f"OHLCV_panel_{folder_nbr}.feather"))
+    OHLCV_panel_iter = pd.read_feather(os.path.join(data_path, "Panel_Data_EU", f"OHLCV_panel_{folder_nbr}.feather"))
     static_iter      = pd.read_excel(os.path.join(folder_path, f'static_{folder_nbr}.xlsx'), engine='openpyxl')
     timeseries_dfs.append(OHLCV_panel_iter)
     static_dfs.append(static_iter)
@@ -26,6 +26,9 @@ for i in tqdm(range(1, nof_subfolders + 1), desc="Load data"):
 statics = pd.concat(static_dfs, axis=0, ignore_index=True)
 statics.reset_index(drop=True, inplace=True)
 
+########################################################################################################################
+## Define datatypes:
+########################################################################################################################
 delist_str = statics["ENAME"].str.extract(r"DELIST\.(\d{2}/\d{2}/\d{2})")[0]
 statics["Delisting Date"] = pd.to_datetime(delist_str, format="%d/%m/%y", errors="coerce")
 
@@ -40,16 +43,14 @@ OHLCV_panel = pd.concat(timeseries_dfs, axis=0, ignore_index=True)
 OHLCV_panel.sort_values(by=["Date", "Stock"], inplace=True)
 OHLCV_panel.reset_index(drop=True, inplace=True)
 
-logging.info(f"Number of rows before removing duplicate Stock-Date observations: {OHLCV_panel.shape[0]}")
 OHLCV_panel = OHLCV_panel.drop_duplicates(subset=["Stock", "Date"], keep="first")
-logging.info(f"Number of rows after removing duplicate Stock-Date observations: {OHLCV_panel.shape[0]}")
 
 OHLCV_panel.loc[:, 'Stock'] = OHLCV_panel['Stock'].astype(str)
 OHLCV_panel.loc[:, 'Stock'] = OHLCV_panel['Stock'].str.strip()
 statics.loc[:, 'DSCD']      = statics['DSCD'].str.strip()
 
 mask = OHLCV_panel["ReturnIndex"] < 1e-6
-logging.info(f"Frequency of ReturnIndex observations with extreme small values: {mask.sum()/OHLCV_panel.shape[0]:.6f}")
+mask.sum()
 
 OHLCV_panel.loc[mask, "ReturnIndex"] = np.nan
 
@@ -58,19 +59,61 @@ OHLCV_panel["Return"] = (
     .transform(lambda x: x / x.shift(1) - 1)
 )
 
-logging.info(f"Number of companies before removing non-regional companies: {statics.shape[0]}")
-statics = statics[statics['GEOGN'] == 'UNITED STATES']
-logging.info(f"Number of companies after removing non-regional companies: {statics.shape[0]}")
+########################################################################################################################
+## Filter data:
+########################################################################################################################
+## Generic filters
+########################################################################################################################
+OHLCV_panel = OHLCV_panel[OHLCV_panel['Date'] <= '2025-01-31']
+OHLCV_panel = OHLCV_panel[OHLCV_panel['Date'] > '1994-01-01']
+
+
+# Filter (11) - Already done in beginning:
+# Remove stocks where RI is unavailable.
+# -> This is already done in AttProj1_ProcessRawData
+
+# Drop unknown country codes:
+statics = statics[statics['GEOGN'].notna() & (statics['GEOGN'] != 'nan')  & (statics['GEOGN'] != 'UNITED STATES')]
+# Guernsey -> Checked manually. (Sancus Lending Group).
+statics.loc[statics["GEOGN"] == "GUERNSEY", "GEOGN"] = "UNITED KINGDOM"
+
+########################################################################################################################
+## Filter data:
+########################################################################################################################
+## Generic filters
+########################################################################################################################
+OHLCV_panel = OHLCV_panel[OHLCV_panel['Date'] <= '2025-01-31']
+OHLCV_panel = OHLCV_panel[OHLCV_panel['Date'] > '1994-01-01']
+
+
+# Filter (11) - Already done in beginning:
+# Remove stocks where RI is unavailable.
+# -> This is already done in AttProj1_ProcessRawData
+
+# Drop unknown country codes:
+statics = statics[statics['GEOGN'].notna() & (statics['GEOGN'] != 'nan')  & (statics['GEOGN'] != 'UNITED STATES')]
+# Guernsey -> Checked manually. (Sancus Lending Group).
+statics.loc[statics["GEOGN"] == "GUERNSEY", "GEOGN"] = "UNITED KINGDOM"
 
 ########################################################################################################################
 ## Filters based on static data
 ########################################################################################################################
 # Filter (1) - Equity filter:
-OHLCV_panel = DSPreprocess.filter_non_common_stocks(OHLCV_panel, statics, country='UNITED STATES')
+panels_temp = [
+    DSPreprocess.filter_non_common_stocks(
+        OHLCV_panel[OHLCV_panel['Stock'].isin(statics.loc[statics['GEOGN'] == country, 'DSCD'])], statics, country=country)
+    for country in statics['GEOGN'].unique()
+]
+OHLCV_panel = pd.concat(panels_temp, ignore_index=True)
 
 
 # Filter (2) - Cross-listing filter:
-OHLCV_panel = DSPreprocess.filter_cross_listings(OHLCV_panel, statics, country='UNITED STATES')
+panels_temp = [
+    DSPreprocess.filter_cross_listings(
+        OHLCV_panel[OHLCV_panel['Stock'].isin(statics.loc[statics['GEOGN'] == country, 'DSCD'])], statics, country=country)
+    for country in statics['GEOGN'].unique()
+]
+OHLCV_panel = pd.concat(panels_temp, ignore_index=True)
 
 
 # Filter (3): Duplicate LOC Codes
@@ -82,11 +125,23 @@ OHLCV_panel = OHLCV_panel[OHLCV_panel.Stock.isin(statics.DSCD.unique())]
 
 
 # Filter (5) - Stocks in foreign currencies:
-OHLCV_panel = DSPreprocess.filter_foreign_currency_stocks(OHLCV_panel, statics, country='UNITED STATES')
+panels_temp = [
+    DSPreprocess.filter_foreign_currency_stocks(
+        OHLCV_panel[OHLCV_panel['Stock'].isin(statics.loc[statics['GEOGN'] == country, 'DSCD'])], statics, country=country)
+    for country in statics['GEOGN'].unique()
+]
+
+OHLCV_panel = pd.concat(panels_temp, ignore_index=True)
 
 
-# Filter (17) - Survivorship bias (obsolete for US data, because our dataset starts in 1993):
-# OHLCV_panel = DSPreprocess.filter_surivorship_bias(OHLCV_panel, statics, country='UNITED STATES')
+# Filter (17) - Survivorship bias:
+panels_temp = [
+    DSPreprocess.filter_surivorship_bias(
+        OHLCV_panel[OHLCV_panel['Stock'].isin(statics.loc[statics['GEOGN'] == country, 'DSCD'])], statics, country=country)
+    for country in statics['GEOGN'].unique()
+]
+
+OHLCV_panel = pd.concat(panels_temp, ignore_index=True)
 
 
 ########################################################################################################################
@@ -95,6 +150,7 @@ OHLCV_panel = DSPreprocess.filter_foreign_currency_stocks(OHLCV_panel, statics, 
 # Filter (7) - :
 # Remove stocks of which more than 98% of non-zero mean returns are either positive or negative
 OHLCV_panel = DSPreprocess.filter_implausible_returns(OHLCV_panel)
+
 
 ########################################################################################################################
 ## Stockday filters:
@@ -105,7 +161,7 @@ OHLCV_panel = DSPreprocess.filter_padded_values_delistings(OHLCV_panel, statics)
 
 
 # Filter (8):
-# Remove stocks for which the returns are zero in more than 95% of their sample (After applying filter (13).
+# Remove stocks for which the returns are zero in more than 95% of their sample (After applying filter (13)).
 OHLCV_panel = DSPreprocess.filter_zero_return_stocks(OHLCV_panel)
 
 
@@ -152,65 +208,39 @@ OHLCV_panel = DSPreprocess.filter_implausible_prices(OHLCV_panel)
 # Filter (No trading activity - Chaieb et al. (2021) JoFE)
 OHLCV_panel = DSPreprocess.filter_no_trading_activity(OHLCV_panel)
 
-#
-# # Filter (Own - Extreme returns)
-# # OHLCV_panel = DSPreprocess.filter_extreme_returns(OHLCV_panel, lower=0.00, upper=0.999)
-# OHLCV_panel = DSPreprocess.filter_extreme_returns2(OHLCV_panel, n_std=5) # Less aggressive than above version.
-
 
 # Filter (Own - NA filter) - Drop all rows before they are populated for the first time and apply forward + backward fill.
-OHLCV_panel = DSPreprocess.handle_missings(OHLCV_panel, statics, country='UNITED STATES')
+panels_temp = [
+    DSPreprocess.handle_missings(
+        OHLCV_panel[OHLCV_panel['Stock'].isin(statics.loc[statics['GEOGN'] == country, 'DSCD'])], statics, country=country)
+    for country in statics['GEOGN'].unique()
+]
+OHLCV_panel = pd.concat(panels_temp, ignore_index=True)
 
-
-########################################################################################################################
-# Manual removal of implausibilities:
-########################################################################################################################
-# 1.) AgEagle Aerial Systems, Inc. (680683). Remove observations before foundation date.
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel['Stock'] == "680683") & (OHLCV_panel['Date'] < '2010-01-01'))]
-
-
-# 2.) Strange prices due to stock splits / reverse splits
-OHLCV_panel = OHLCV_panel[~(OHLCV_panel["Stock"]  == "872328")]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "9364PF") & (OHLCV_panel["Date"] == "2020-07-13"))]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "50259R") & (OHLCV_panel["Date"] == "2018-01-04"))]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "67684T") & (OHLCV_panel["Date"] == "2023-08-07"))]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "2566DU") & (OHLCV_panel["Date"] == "2024-06-04"))]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "28355P") & (OHLCV_panel["Date"] == "2008-12-17"))]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "2634G3") & (OHLCV_panel["Date"] == "2024-03-27"))]
-OHLCV_panel = OHLCV_panel[~((OHLCV_panel["Stock"] == "32650J") & (OHLCV_panel["Date"] == "2009-05-07"))]
-
-
-# 3.) Implausible returns due to high amount of missings:
-OHLCV_panel = OHLCV_panel[~(OHLCV_panel["Stock"] == "7076TJ")]
-OHLCV_panel = OHLCV_panel[~(OHLCV_panel["Stock"] == "7076TK")]
-
-
-# 4.) Seems to be some sort of dividend split, that was incorrectly labeled as a stock:
-OHLCV_panel = OHLCV_panel[~(OHLCV_panel["Stock"] == "92238K")]
-
-
-# 5.) Delete day with unusual drop in listed firms:
-# OHLCV_panel = OHLCV_panel[~(OHLCV_panel["Date"] == '1995-05-26')]
 
 # Filter (18) - Adjustment inconsistencies.
 OHLCV_panel_temp = DSPreprocess.filter_adjustment_inconsistencies(OHLCV_panel, threshold=0.05)
 
 
 # Filter (21) - Penny stocks.
-OHLCV_panel = DSPreprocess.filter_penny_stocks(OHLCV_panel, 0.10)
+OHLCV_panel_temp = DSPreprocess.filter_penny_stocks(OHLCV_panel_temp, 0.10)
+
+
+# Filter (6) - Small-country filter (Not relevant for US dataset).
+OHLCV_panel_temp, statics = DSPreprocess.filter_countries_with_few_stocks(OHLCV_panel_temp, statics)
 
 
 # Filter (Own - Extreme returns)
 # OHLCV_panel = DSPreprocess.filter_extreme_returns(OHLCV_panel, lower=0.00, upper=0.999)
-OHLCV_panel = DSPreprocess.filter_extreme_returns2(OHLCV_panel, n_std=5) # Less aggressive than above version.
+OHLCV_panel_temp = DSPreprocess.filter_extreme_returns2(OHLCV_panel_temp, n_std=5) # Less aggressive than above version.
 
 
 # Filter (12) - Filters the panel to include only stocks with sufficient observation history
-OHLCV_panel = DSPreprocess.filter_short_history_stocks(OHLCV_panel, threshold=120)
-
+OHLCV_panel_temp = DSPreprocess.filter_short_history_stocks(OHLCV_panel_temp, threshold=120)
 
 OHLCV_panel.replace([np.inf, -np.inf], np.nan, inplace=True)
 OHLCV_panel_final = OHLCV_panel.dropna(subset=['Return'])
+
 
 # extract companies which are in the final filtered data set
 statics_for_filtered = statics[statics.DSCD.isin(OHLCV_panel_final.Stock.unique().tolist())]
@@ -219,3 +249,4 @@ statics_for_filtered = statics[statics.DSCD.isin(OHLCV_panel_final.Stock.unique(
 logging.info("Saving data and static information for remaining companies.")
 OHLCV_panel_final.to_feather(os.path.join(data_path, save_dir, "Financial_base_data_panel_filtered.feather"))
 statics_for_filtered.to_csv(os.path.join(data_path, save_dir, "statics_filtered.csv"), index = False)
+
